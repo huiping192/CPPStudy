4094d90ddd52fab1a57cd13aa271fa648b9f9687
huiping.guo <huiping.guo@dena.jp>
2020-02-21 02:20:42 +0900

    add samples

---
+++ 02_input_output/input_output.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Huiping Guo on 2020/02/21.
+//
+
+
+#include <iostream>
+//
+//int main() {
+//    int number;
+//    std::cout << "put a int number:";
+//    std::cin >> number;
+//
+//    std::cout << "number:" << number;
+//
+//    return 0;
+//}
---
+++ 03_add_number/add_number.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Huiping Guo on 2020/02/21.
+//
+
+#include <iostream>
+
+int main() {
+    int i,j,sum;
+
+    std::cout << "put two int number";
+
+    std::cin >> i >> j;
+
+    sum = i + j;
+
+    std::cout << i << " + " << j << " = " << sum;
+}
