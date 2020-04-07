d4a306806769eb905ab8cab17c4dbb2d820ba120
huiping_guo <huiping.guo@dena.jp>
2020-04-08 02:01:06 +0900

    add void* sample

--- 07_pointer/pointer.cpp
+++ 07_pointer/pointer.cpp
@@ -1,30 +1,43 @@
 //
 // Created by Huiping Guo on 2020/04/06.
 //
+#include <iostream>
 
 
 void add(int a,int b, int *c) {
     *c = a + b;
 }
 
-
-int main() {
-
+void samplePoint() {
     int x = 5;
     int *ptr = &x;
 
     *ptr = 10;
 
     int y = 6;
-
     ptr = &y;
 
     auto a = *ptr;
 
-
     int c;
 //    int *c = nullptr;  // wrong
     add(1,2,&c);
+}
+
+void voidSample() {
+    int a = 1;
+
+    void* b  = &a;
+
+    int* c = static_cast<int*>(b);
+
+    std::cout << *c;
+}
+
+
+int main() {
+
+    voidSample();
 
     return 1;
 }
